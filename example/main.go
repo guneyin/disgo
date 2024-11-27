cc402a0cac72c8196ac97275a91428aaf06b01e1
Hüseyin Güney <guneyin@gmail.com>
2024-11-27 12:58:23 +0300

    example updated

--- example/main.go
+++ example/main.go
@@ -18,6 +18,30 @@ type handler struct {
 }
 
 func main() {
+	gc := provider.GoogleConfig{
+		ApiKey:       os.Getenv("DSG_GOOGLE_API_KEY"),
+		ClientID:     os.Getenv("DSG_GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("DSG_GOOGLE_SECRET"),
+		CallBackUrl:  "http://localhost:8080/auth/google/callback",
+	}
+
+	accessToken := os.Getenv("DSG_GOOGLE_OAUTH2_TOKEN")
+
+	gp := provider.NewGoogle(gc, []byte(accessToken))
+	user, err := gp.GetAuthorizedUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	userData, _ := json.MarshalIndent(user, "", " ")
+	_ = fmt.Sprintf("%-v", string(userData))
+
+	dirs, err := gp.GetDirectoryList("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dirsData, _ := json.MarshalIndent(dirs, "", " ")
+	_ = fmt.Sprintf("%-v", string(dirsData))
+
 	ctx := context.Background()
 
 	err := godotenv.Load(".env.local")
